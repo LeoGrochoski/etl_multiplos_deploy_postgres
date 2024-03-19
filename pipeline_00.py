a8d91c74ebe1050d66133db8aafa169b4010c3a2
LeoGrochoski <leogrochoski10@gmail.com>
2024-03-18 21:31:51 -0300

    add arquivos faltantes

--- pipeline_00.py
+++ pipeline_00.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 from duckdb import DuckDBPyRelation
 from pandas import DataFrame
 
+load_dotenv()
+
 def baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local):
     os.makedirs(diretorio_local, exist_ok=True)
     gdown.download_folder(url_pasta, output=diretorio_local, quiet=False, use_cookies=False)
@@ -24,21 +26,25 @@ def listar_arquivos_csv(diretorio):
 #Função que le arquivos e retorna um dataframe duckdb
 def ler_csv(caminho_arquivo):
     dataframe_duckdb = duckdb.read_csv(caminho_arquivo)
-    print(dataframe_duckdb)
-    print(type(dataframe_duckdb))
     return dataframe_duckdb
 
 def transformacao(df: DuckDBPyRelation) -> DataFrame:
     df_transformado =duckdb.sql("SELECT *, quantidade * valor AS total_vendas FROM df").df()
-    print(df_transformado)
     return df_transformado
 
+def salvar_banco_postgres(df_duckdb, tabela):
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    engine = create_engine(DATABASE_URL)
+    df_duckdb.to_sql(tabela, con=engine, if_exists='append', index=False)
 
 if __name__ == "__main__":
     url_pasta ='https://drive.google.com/drive/folders/19flL9P8UV9aSu4iQtM6Ymv-77VtFcECP'
     diretorio_local = './pasta_gdown'
     # baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local)
-    arquivos = listar_arquivos_csv(diretorio_local)
-    duckdb_dataframe = ler_csv(arquivos)
-    transformacao(duckdb_dataframe)
+    lista_de_arquivos = listar_arquivos_csv(diretorio_local)
+
+    for caminho_do_arquivo in lista_de_arquivos:
+        duck_db_df = ler_csv(caminho_do_arquivo)
+        pandas_df_transformado = transformacao(duck_db_df)
+        salvar_banco_postgres(pandas_df_transformado, "vendas_calculado")
 
